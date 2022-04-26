c1d0fb937ffdc02817acf7d77e744cbad11fcae1
Everton Ferens <evertonferens0@gmail.com>
2022-04-26 10:19:58 -0300

    Create Entity post com user aninhado

--- src/main/java/com/ferens/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ferens/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.ferens.workshopmongo.config;
 
+import com.ferens.workshopmongo.domain.Post;
 import com.ferens.workshopmongo.domain.User;
+import com.ferens.workshopmongo.repository.PostRepository;
 import com.ferens.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,16 +18,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para Sao paulo abraços! ", maria);
+        Post post2 = new Post(null,sdf.parse("21/03/2018"),"Bom dia","acordei feliz hoje! ", maria);
+
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        postRepository.saveAll(Arrays.asList(post1,post2));
 
     }
 }
---
+++ src/main/java/com/ferens/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.ferens.workshopmongo.repository;
+
+
+import com.ferens.workshopmongo.domain.Post;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+
+}
