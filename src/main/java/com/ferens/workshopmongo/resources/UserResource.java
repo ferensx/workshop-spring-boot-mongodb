f8e8fd090930b12e2d95da871fc95710b0819133
Everton Ferens <evertonferens0@gmail.com>
2022-04-26 06:44:12 -0300

    ADD DTO

--- src/main/java/com/ferens/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ferens/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ferens.workshopmongo.resources;
 
 import com.ferens.workshopmongo.domain.User;
+import com.ferens.workshopmongo.dto.UserDTO;
 import com.ferens.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
     private UserService service;
 
 
-    @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
