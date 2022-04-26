10692516a82210ee0cd7a5a001e6821b55dc0e20
Everton Ferens <evertonferens0@gmail.com>
2022-04-26 08:32:53 -0300

    Insert de usuario com POST

--- src/main/java/com/ferens/workshopmongo/services/UserService.java
+++ src/main/java/com/ferens/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ferens.workshopmongo.services;
 
 import com.ferens.workshopmongo.domain.User;
+import com.ferens.workshopmongo.dto.UserDTO;
 import com.ferens.workshopmongo.repository.UserRepository;
 import com.ferens.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +21,21 @@ public class UserService {
 
 
     }
-        // LIGADO COM OS EXCEPTIONS DE SERVICE E  RESOURCES .ESSA MSG APARECE NO ERORR 404
+
+    // LIGADO COM OS EXCEPTIONS DE SERVICE E  RESOURCES .ESSA MSG APARECE NO ERORR 404
     public User findById(String id) {
 
         Optional<User> obj = repo.findById(id);
-        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
+    }
+//inserir usario
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
