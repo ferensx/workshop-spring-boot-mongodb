42eaf6ea2989397ce2f64cff209feed716a73afa
Everton Ferens <evertonferens0@gmail.com>
2022-05-03 08:58:20 -0300

    Varios criterios

--- src/main/java/com/ferens/workshopmongo/services/PostService.java
+++ src/main/java/com/ferens/workshopmongo/services/PostService.java
@@ -8,6 +8,7 @@ import com.ferens.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,8 +25,13 @@ public class PostService {
             return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
         }
 
-        public List<Post> findByTitle(String text){
-        return repo.findByTitle(text);
+        public List<Post> findByTitle(String text) {
+            return repo.findByTitle(text);
+        }
+        public List<Post> fullSearch(String text , Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime()+ 24*60* 60*1000);
+        return repo.fullSearch(text,minDate,maxDate);
 
-    }}
+        }
+    }
 
