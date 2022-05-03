88ea4d5bad5b301181c63ef612c10f5ec53935a3
Everton Ferens <evertonferens0@gmail.com>
2022-05-03 08:16:33 -0300

    Consulta com @Query

--- src/main/java/com/ferens/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ferens/workshopmongo/repository/PostRepository.java
@@ -4,13 +4,17 @@ package com.ferens.workshopmongo.repository;
 import com.ferens.workshopmongo.domain.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post>findByTitleContainingIgnoreCase(String text);
+    @Query("{ 'title': { $regex: ?0,$options: 'i'} }")
+    List<Post> findByTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 
 
 }
